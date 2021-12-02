b40a830cbd5619a3515a89c209683c49c60dc26a
Taran Ulrich <65303833+TaranYourAss@users.noreply.github.com>
2021-12-02 02:37:35 -0600

    Update tor_ip_collector.py
    Made the script more modular

--- tor_ip_collector/tor_ip_collector.py
+++ tor_ip_collector/tor_ip_collector.py
@@ -40,10 +40,14 @@ def pulldata (target_url, ip_type):
 
 def main():
     parser = argparse.ArgumentParser()
-    #parser.add_argument('--host', help='host , REQUIRED', required=True)
-    parser.add_argument('--apikey', help='password , REQUIRED', required=True)
+    parser.add_argument('--host', help='Helix URL: https://apps.fireeye.com/helix/id/<helix_id> - REQUIRED', required=True)
+    parser.add_argument('--apikey', help='Api Key - REQUIRED', required=True)
+    parser.add_argument('--listid', help='Id of the Helix list - REQUIRED', required=True)
+    parser.add_argument('--ipv4', action='store_true', help='Specifies to only collect ipv4 addresses', required=False)
+    parser.add_argument('--ipv6', action='store_true', help='Specifies to only collect ipv6 addresses', required=False)
     args, unknown = parser.parse_known_args()
-    #host = args.host.rstrip('/')
+    host = args.host.rstrip('/')
+
 
     #headers for authentication to Helix
     headers = {
@@ -53,12 +57,12 @@ def main():
     }
 
     #query to POST JSON disctionary list into Helix
-    #import_list = "{}/api/v3/lists/{}/items/import".format(host, "592155")
-    import_list = "https://apps.fireeye.com/helix/id/XXXXXX/api/v3/lists/XXXXX/items/import"
+    import_list = "{}/api/v3/lists/{}/items/import".format(host, listid)
+    #import_list = "https://apps.fireeye.com/helix/id/XXXXXX/api/v3/lists/XXXXX/items/import"
 
     #query to DELETE all items in the tor_ips list in Helix
-    #delete_list = "{}/api/v3/lists/{}/items".format(host, "592155")
-    delete_list = "https://apps.fireeye.com/helix/id/XXXXX/api/v3/lists/XXXXXX/items"
+    delete_list = "{}/api/v3/lists/{}/items".format(host, listid)
+    #delete_list = "https://apps.fireeye.com/helix/id/XXXXX/api/v3/lists/XXXXXX/items"
 
     #injects todays date into the notes for every TOR IP entry in Helix
     notes = "Added: " + str(date.today())
@@ -67,11 +71,22 @@ def main():
     json_data = []
     #--end of variables--
 
-    #1 - Collect TOR IPs
-    tor_ipv4 = pulldata(target_url='https://raw.githubusercontent.com/SecOps-Institute/Tor-IP-Addresses/master/tor-nodes.lst', ip_type="ipv4")
-    tor_ipv4.sort()
-    tor_ipv6 = pulldata(target_url='https://raw.githubusercontent.com/SecOps-Institute/Tor-IP-Addresses/master/tor-nodes.lst', ip_type="ipv6")
-    tor_ipv6.sort()
+    #1 - Collect TOR IPs, creates a list of of the IPs in JSON format 
+    node_list_url = 'https://raw.githubusercontent.com/SecOps-Institute/Tor-IP-Addresses/master/tor-nodes.lst'
+    tor_ipv4 = []
+    tor_ipv6 = []
+    if ipv4:
+        tor_ipv4 = pulldata(target_url=node_list_url, ip_type="ipv4")
+        tor_ipv4.sort()
+    elif ipv6:
+        tor_ipv6 = pulldata(target_url=node_list_url, ip_type="ipv6")
+        tor_ipv6.sort()
+    else:
+        tor_ipv6 = pulldata(target_url=node_list_url, ip_type="ipv6")
+        tor_ipv6.sort()
+        tor_ipv4 = pulldata(target_url=node_list_url, ip_type="ipv4")
+        tor_ipv4.sort()
+
 
     #2 - Purge list of IPs in Helix
     try:
@@ -84,10 +99,12 @@ def main():
         exit(-1)
 
     #3 - Create list of JSON dictionaries from updated TOR IP list
-    for ip in tor_ipv4:
-        json_data.append({"value": ip, "type": "ipv4", "risk":"Medium", "notes":notes})
-    for ip in tor_ipv6:
-        json_data.append({"value": ip, "type": "ipv6", "risk":"Medium", "notes":notes})
+    if tor_ipv4:
+        for ip in tor_ipv4:
+            json_data.append({"value": ip, "type": "ipv4", "risk":"Medium", "notes":notes})
+    if tor_ipv6:
+        for ip in tor_ipv6:
+            json_data.append({"value": ip, "type": "ipv6", "risk":"Medium", "notes":notes})
 
     #4 - POST JSON data into Helix
     try:
@@ -95,12 +112,11 @@ def main():
         if post_response.status_code not in range(200, 300): 
             raise PostListFailed
     except PostListFailed:
-        print("POSTing the tor_ips list failed")
+        print("POSTing the list of tor IPs failed")
         print(post_response)
         exit(-1)
 
 if __name__ == "__main__":
 	main()
     
-#test_data = [{"value":"127.0.0.1","type":"ipv4","risk":"Medium","notes":""}, {"value":"127.0.0.2","type":"ipv4","risk":"Medium","notes":""}]
-
+#test_data = [{"value":"127.0.0.1","type":"ipv4","risk":"Medium","notes":"testing"}, {"value":"127.0.0.2","type":"ipv4","risk":"Medium","notes":"test"}]
